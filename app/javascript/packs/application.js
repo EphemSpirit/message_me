4193d9d348b6fbef69da772bd639acecc608cf9c
Drew Hund <languageinvestigator@gmail.com>
2020-11-28 18:07:26 -0500

    made js functions for chat window

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,12 +15,32 @@ scroll_bottom = function() {
   }
 }
 
+clear_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      e.target.value = '';
+    }
+  });
+}
+
+// submit_message = function() {
+//   $('#message_body').on('keydown', function(e) {
+//     if (e.keyCode == 13) {
+//       $('button').click();
+//       e.target.value = "";
+//     }
+//   })
+// }
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  //submit_message();
   scroll_bottom();
+  clear_message();
 })
 
 
